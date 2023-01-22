03f7f6cf7394053fea42a44edbe6d243f9bb0749
Anthony Dentinger <a.dentinger@gmail.com>
2023-01-22 14:55:27 -0500

    Create CMakePresets.json in lib1/

--- lib1/cmake-commands.sh
+++ lib1/cmake-commands.sh
@@ -1,1 +1,4 @@
-cd "$HOME/Git/foo/lib1" && rm -rf build include/ bin lib cmake && mkdir build && cd build/ && cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$(realpath "$(pwd)/..")" -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DBUILD_SHARED_LIBS=Off .. && cmake --build . && cmake --install .
+cmake --workflow --preset ninja-release
+
+# cd "$HOME/Git/foo/lib1" && rm -rf build include/ bin lib cmake && mkdir build && cd build/ && cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$(realpath "$(pwd)/..")" -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DBUILD_SHARED_LIBS=Off .. && cmake --build . && cmake --install .
+
