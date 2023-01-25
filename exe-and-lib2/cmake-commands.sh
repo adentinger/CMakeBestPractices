8d173c1b5020eea9ad3d5dba1b3ba14cd738676b
Anthony Dentinger <adentinger.dev@gmail.com>
2023-01-24 20:45:07 -0500

    Add #!/bin/bash to cmake-commands.sh files

--- exe-and-lib2/cmake-commands.sh
+++ exe-and-lib2/cmake-commands.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cmake --workflow --preset ninja-release && cmake --install build
 
 # cd "$HOME/Git/foo/exe-and-lib2/" && rm -rf build include bin lib cmake && mkdir build && cd build && cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$(realpath "$(pwd)/..")" -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DBUILD_SHARED_LIBS=Off -DLIB1_INSTALL_DIR="$(realpath "$(pwd)/../../lib1")" .. && cmake --build . && cmake --install .
