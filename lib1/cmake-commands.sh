aa6cbf30687c9820dad37356e6700cecf19a442f
Anthony Dentinger <adentinger.dev@gmail.com>
2023-01-27 23:45:42 -0500

    Make executable run with DLLs
    Still have to fix Unix shared objects.

--- lib1/cmake-commands.sh
+++ lib1/cmake-commands.sh
@@ -2,7 +2,8 @@
 
 SCRIPT_DIR="$(cd "$(basename "${BASH_SOURCE[0]}")" 2>/dev/null && pwd)"
 
-cd "${SCRIPT_DIR}" && cmake --workflow --preset ninja-release && cmake --install build
+# cd "${SCRIPT_DIR}" && cmake --workflow --preset ninja-release-static && cmake --install build
+cd "${SCRIPT_DIR}" && cmake --workflow --preset ninja-release-shared && cmake --install build
 
 # cd "$HOME/Git/foo/lib1" && rm -rf build include/ bin lib cmake && mkdir build && cd build/ && cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$(realpath "$(pwd)/..")" -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DBUILD_SHARED_LIBS=Off .. && cmake --build . && cmake --install .
 
