b4e5b6df4d987ab8d3e0999930125f0eed06d4b9
Anthony Dentinger <a.dentinger@gmail.com>
2023-01-27 23:45:42 -0500

    Make executable run with DLLs
    Still have to fix Unix shared objects.

--- exe-and-lib2/lib2/include/lib2/defines.h
+++ exe-and-lib2/lib2/include/lib2/defines.h
@@ -2,13 +2,13 @@
 #define LIB2_DEFINES_H_
 
 // https://cmake.org/cmake/help/latest/guide/tutorial/Selecting%20Static%20or%20Shared%20Libraries.html#mathfunctions-mathfunctions-h
-#if defined(_WIN32)
+#if defined(_WIN32) && defined(LIB2_SHARED)
 #  if defined(LIB2_EXPORT)
 #    define LIB2_SHRSYM __declspec(dllexport)
 #  else
 #    define LIB2_SHRSYM __declspec(dllimport)
 #  endif
-#else // non windows
+#else // non-DLL
 #  define LIB2_SHRSYM
 #endif
 
--- lib1/include/lib1/defines.h
+++ lib1/include/lib1/defines.h
@@ -2,13 +2,13 @@
 #define LIB1_DEFINES_H_
 
 // https://cmake.org/cmake/help/latest/guide/tutorial/Selecting%20Static%20or%20Shared%20Libraries.html#mathfunctions-mathfunctions-h
-#if defined(_WIN32)
+#if defined(_WIN32) && defined(LIB1_SHARED)
 #  if defined(LIB1_EXPORT)
 #    define LIB1_SHRSYM __declspec(dllexport)
 #  else
 #    define LIB1_SHRSYM __declspec(dllimport)
 #  endif
-#else // non windows
+#else // non-DLL
 #  define LIB1_SHRSYM
 #endif
 
