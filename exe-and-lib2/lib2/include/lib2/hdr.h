44d52810a9b79727fa2eb4a508797934818949b6
Anthony Dentinger <a.dentinger@gmail.com>
2023-01-30 23:12:09 -0500

    Implement fibo_sum() and make it pass clang-tidy

--- exe-and-lib2/lib2/include/lib2/hdr.h
+++ exe-and-lib2/lib2/include/lib2/hdr.h
@@ -1,9 +1,13 @@
 #ifndef LIB2_HDR_H
 #define LIB2_HDR_H
 
+#include <cstddef>
+#include <inttypes.h> // NOLINT(modernize-deprecated-headers) Just "uint<N>_t".
+
 #include "lib2/defines.h"
+
 namespace lib2 {
-LIB2_SHRSYM void f();
+LIB2_SHRSYM std::size_t fibo_sum(uint32_t n);
 } // namespace lib2
 
 #endif // !LIB2_HDR_H
