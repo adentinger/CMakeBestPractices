3606760547247f3273f36c576aac104472235d8e
Anthony Dentinger <a.dentinger@gmail.com>
2023-02-04 18:10:47 -0500

    Integrate SilClangFormat into lib1

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -2,27 +2,40 @@
 # SPDX-License-Identifier: MIT
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" 2>/dev/null && pwd)"
+SILCLANGFORMAT_WORKFLOW=ninja
 LIB1_WORKFLOW=ninja-release-shared
 LIB2EXE_WORKFLOW=ninja-release-shared
 
+big_message() {
+	echo
+	echo "--------"
+	echo "$@"
+	echo "--------"
+	echo
+}
+
 cd "${SCRIPT_DIR}"
 
-rm -rf {lib1,exe-and-lib2}/build install
-cd lib1
+rm -rf {SilClangFormat,lib1,exe-and-lib2}/build install
+big_message "SilClangFormat"
+cd "${SCRIPT_DIR}"/SilClangFormat
+"${CMAKE_BIN_DIR}"cmake --workflow --preset "${SILCLANGFORMAT_WORKFLOW}"
+# TODO Add this to workflow
+"${CMAKE_BIN_DIR}"ctest --test-dir build
+"${CMAKE_BIN_DIR}"cmake --install build
+big_message "lib1"
+cd "${SCRIPT_DIR}"/lib1
 "${CMAKE_BIN_DIR}"cmake --workflow --preset "${LIB1_WORKFLOW}"
 # TODO Add this to workflow
 "${CMAKE_BIN_DIR}"ctest --test-dir build
 "${CMAKE_BIN_DIR}"cmake --install build
+big_message "exe-and-lib2"
 cd "${SCRIPT_DIR}"/exe-and-lib2
 "${CMAKE_BIN_DIR}"cmake --workflow --preset "${LIB2EXE_WORKFLOW}"
 # TODO Add this to workflow
 "${CMAKE_BIN_DIR}"ctest --test-dir build
 "${CMAKE_BIN_DIR}"cmake --install build
+big_message "Running executable"
 cd "${SCRIPT_DIR}"
-echo
-echo "--------"
-echo "Running executable"
-echo "--------"
-echo
 ./install/bin/exe
 
