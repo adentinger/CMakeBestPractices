4c2686f8f5f79d15e35bbb1249cd22f2816539ba
Anthony Dentinger <a.dentinger@gmail.com>
2023-02-04 20:05:41 -0500

    Tweak compile-and-run.sh

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -19,19 +19,19 @@ echo "Assuming cmake command is \"${CMAKE_BIN_DIR}cmake\""
 cd "${SCRIPT_DIR}"
 
 rm -rf {SilClangFormat,lib1,exe-and-lib2}/build install
-big_message "SilClangFormat"
+big_message "SilClangFormat workflow ${SILCLANGFORMAT_WORKFLOW}"
 cd "${SCRIPT_DIR}"/SilClangFormat
 "${CMAKE_BIN_DIR}"cmake --workflow --preset "${SILCLANGFORMAT_WORKFLOW}"
 # TODO Add this to workflow
 "${CMAKE_BIN_DIR}"ctest --test-dir build
 "${CMAKE_BIN_DIR}"cmake --install build
-big_message "lib1"
+big_message "lib1 workflow ${LIB1_WORKFLOW}"
 cd "${SCRIPT_DIR}"/lib1
 "${CMAKE_BIN_DIR}"cmake --workflow --preset "${LIB1_WORKFLOW}"
 # TODO Add this to workflow
 "${CMAKE_BIN_DIR}"ctest --test-dir build
 "${CMAKE_BIN_DIR}"cmake --install build
-big_message "exe-and-lib2"
+big_message "exe-and-lib2 workflow ${LIB2EXE_WORKFLOW}"
 cd "${SCRIPT_DIR}"/exe-and-lib2
 "${CMAKE_BIN_DIR}"cmake --workflow --preset "${LIB2EXE_WORKFLOW}"
 # TODO Add this to workflow
