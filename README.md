941e5c52fea0427820b0744c41fca24bde239aa7
Anthony Dentinger <a.dentinger@gmail.com>
2023-02-07 22:33:53 -0500

    Remove ninja from preset
    We want the CI to actually use the presets, but the CI also needs to
    try other generators as well, so we can't specify a generator inside
    the presets. Just use whatever is specified in the CMAKE_GENERATOR
    environment variable.

--- README.md
+++ README.md
@@ -3,3 +3,30 @@
 Tests of making libraries and importing them in other CMake projects or
 targets.
 
+## How to use
+
+Requires CMake v3.25. If it such a version is not available on your system, you can download it and then point the `compile-and-run.sh` script to the directory where .
+
+```sh
+CMAKE_GENERATOR=<Specify Generator> \
+	CMAKE_BUILD_TYPE=<Specify build type> \
+    CMAKE_DIR=<path to CMake's dir if cmake is not in PATH; the CMake executable is "${CMAKE_DIR}/bin/cmake"> \
+    ./compile-and-run.sh
+```
+
+For example:
+
+```sh
+CMAKE_GENERATOR=Ninja \
+	CMAKE_BUILD_TYPE=Release \
+    ./compile-and-run.sh
+```
+
+### Multi-config generators
+
+Note that, at the moment, multi-config generators (e.g. `Visual Studio *`, `XCode`, ...) do not work with `compile-and-run.sh`. If you want to compile this project using a multi-config generator, you will have to figure out and specify the CMake flags manually without using `compile-and-run.sh`. To do that, try a single-config generator (e.g. Ninja) and look at the cache variables in `cmake-gui` or `ccmake`, or just look at the generated `CMakeCache.txt` directly.
+
+This is because one of this project's goals is to try out new and shiny CMake stuff, so `compile-and-run.sh` uses CMake's `CMakePresets.json` file to specify configure, build and test presets. In a normal context, this file is meant to be used interactively, not through a script, but, well, we really want to try out the new and shiny, so we want to build through that.
+
+Unfortunately, externally via an environment variable, we can't find a way to provide the config (Release/Debug/RelWithDebInfo/...) to build, test, and install, except maybe if we used CMake's [CMAKE_DEFAULT_BUILD_TYPE](https://cmake.org/cmake/help/latest/variable/CMAKE_DEFAULT_BUILD_TYPE.html#variable:CMAKE_DEFAULT_BUILD_TYPE) cache variable in the presets file and get that variable's value through an environement variable. However, even if we did that, it would only work with the `Ninja Multi-Config` generator because that cache variable is only used by that specific generator. So multi-config generators don't work with the `compile-and-run.sh` script right now.
+
--- compile-and-run.sh
+++ compile-and-run.sh
@@ -2,9 +2,9 @@
 # SPDX-License-Identifier: MIT
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" 2>/dev/null && pwd)"
-SILCLANGFORMAT_WORKFLOW=ninja
-LIB1_WORKFLOW=ninja-shared
-LIB2EXE_WORKFLOW=ninja-shared
+SILCLANGFORMAT_WORKFLOW=default
+LIB1_WORKFLOW=shared
+LIB2EXE_WORKFLOW=shared
 
 big_message() {
 	echo
@@ -14,23 +14,51 @@ big_message() {
 	echo
 }
 
-echo "Assuming cmake command is \"${CMAKE_BIN_DIR}cmake\""
+set_cmake() {
+	if [ ! -z "${CMAKE_DIR}" ]; then
+		export CMAKE_CMD="${CMAKE_DIR}/bin/cmake"
+	else
+		export CMAKE_CMD="$(which cmake)"
+	fi
+	echo CMAKE_CMD: "$CMAKE_CMD"
+}
+
+# The CMakePresets.json files check the CMake version, however cmake itself
+# does not support the presets file until CMake 3.19.
+check_cmake_version() {
+	local min_major=3
+	local min_minor=19
+
+	local version_string="$("${CMAKE_CMD}" --version | grep -oP '(?<=^cmake version ).*')"
+	local major_number="$(echo "${version_string}" | grep -oP '^\d+(?=[\.$])')"
+	local minor_number="$(echo "${version_string}" | grep -oP '(?<=^'"${major_number}"'\.)\d+(?=[\.$])')"
+	if [ "${major_number}" -gt "${min_major}" ] || [ "${major_number}" -eq "${min_major}" ] && [ "${minor_number}" -ge "${min_minor}" ]; then
+		echo "OK, CMake version is ${version_string} >= ${min_major}.${min_minor}."
+	else
+		echo "ERROR, CMake version is ${version_string} which is less than the minimum required of ${min_major}.${min_minor}." >&2
+		exit 1
+	fi
+}
+
+set_cmake
+big_message "Assuming cmake command is \"${CMAKE_CMD}\""
+check_cmake_version
 
 cd "${SCRIPT_DIR}"
 
 rm -rf {SilClangFormat,lib1,exe-and-lib2}/build install
 big_message "SilClangFormat workflow ${SILCLANGFORMAT_WORKFLOW}"
 cd "${SCRIPT_DIR}"/SilClangFormat
-"${CMAKE_BIN_DIR}"cmake --workflow --preset "${SILCLANGFORMAT_WORKFLOW}"
-"${CMAKE_BIN_DIR}"cmake --install build
+"${CMAKE_CMD}" --workflow --preset "${SILCLANGFORMAT_WORKFLOW}"
+"${CMAKE_CMD}" --install build --config "${CMAKE_CONFIG_TYPE}"
 big_message "lib1 workflow ${LIB1_WORKFLOW}"
 cd "${SCRIPT_DIR}"/lib1
-"${CMAKE_BIN_DIR}"cmake --workflow --preset "${LIB1_WORKFLOW}"
-"${CMAKE_BIN_DIR}"cmake --install build
+"${CMAKE_CMD}" --workflow --preset "${LIB1_WORKFLOW}"
+"${CMAKE_CMD}" --install build --config "${CMAKE_CONFIG_TYPE}"
 big_message "exe-and-lib2 workflow ${LIB2EXE_WORKFLOW}"
 cd "${SCRIPT_DIR}"/exe-and-lib2
-"${CMAKE_BIN_DIR}"cmake --workflow --preset "${LIB2EXE_WORKFLOW}"
-"${CMAKE_BIN_DIR}"cmake --install build
+"${CMAKE_CMD}" --workflow --preset "${LIB2EXE_WORKFLOW}"
+"${CMAKE_CMD}" --install build --config "${CMAKE_CONFIG_TYPE}"
 big_message "Running executable"
 cd "${SCRIPT_DIR}"
 ./install/bin/exe
