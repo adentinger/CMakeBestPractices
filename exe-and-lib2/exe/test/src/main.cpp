37c79628c28f3b40af5dae362739507847c795cb
Anthony Dentinger <a.dentinger@gmail.com>
2023-02-22 23:02:52 -0500

    Actually use GTest in lib1, lib2 and exe

--- exe-and-lib2/exe/test/src/main.cpp
+++ exe-and-lib2/exe/test/src/main.cpp
@@ -1,6 +1,9 @@
 #include <inttypes.h> // NOLINT(modernize-deprecated-headers) Just "uint<N>_t".
 
-int main() {
-	return 0;
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
 
