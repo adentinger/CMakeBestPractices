aa09ff91b8276b343abfdad708231adefb357ba6
Anthony Dentinger <a.dentinger@gmail.com>
2023-02-28 19:40:21 -0500

    Make lib1 use an intermediary object library
    This fixes shared libs on Windows

---
+++ lib1/extra/cpp/dummy.cpp
@@ -0,0 +1,10 @@
+// This file is intentionally empty.
+//
+// According to this...
+//
+// https://cmake.org/cmake/help/latest/command/add_library.html#object-libraries
+//
+// ...libraries should always have "at least one real source file [for] any
+// target that references [an object library]". This is the "real source
+// file" in question.
+
