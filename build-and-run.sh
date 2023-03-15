662ee5c1875a71279d49ec2a647baf5e9a478392
Anthony Dentinger <a.dentinger@gmail.com>
2023-03-14 20:56:33 -0400

    Specify 10 build treads

--- build-and-run.sh
+++ build-and-run.sh
@@ -259,24 +259,24 @@ big_message "AdeClangFormat build"
 run_or_print cd "${SOURCE_ROOT_DIR}"/AdeClangFormat
 run_or_print export CMAKE_BUILD_TYPE="${ADECLANGFORMAT_CONFIG}"
 run_or_print "${CMAKE_CMD}" --preset "${ADECLANGFORMAT_CONFIGURE_PRESET}"
-run_or_print "${CMAKE_CMD}" --build build --config "${ADECLANGFORMAT_CONFIG}"
-run_or_print "${CTEST_CMD}" --test-dir build --build-config "${ADECLANGFORMAT_CONFIG}"
+run_or_print "${CMAKE_CMD}" --build build --config "${ADECLANGFORMAT_CONFIG}" -j10
+run_or_print "${CTEST_CMD}" --test-dir build --build-config "${ADECLANGFORMAT_CONFIG}" -j10
 run_or_print "${CMAKE_CMD}" --install build --config "${ADECLANGFORMAT_CONFIG}"
 
 big_message "prj1 workflow ${PRJ1_WORKFLOW}"
 run_or_print cd "${SOURCE_ROOT_DIR}"/prj1
 run_or_print export CMAKE_BUILD_TYPE="${PRJ1_CONFIG}"
 run_or_print "${CMAKE_CMD}" --preset "${PRJ1_CONFIGURE_PRESET}"
-run_or_print "${CMAKE_CMD}" --build build --config "${PRJ1_CONFIG}"
-run_or_print "${CTEST_CMD}" --test-dir build --build-config "${PRJ1_CONFIG}"
+run_or_print "${CMAKE_CMD}" --build build --config "${PRJ1_CONFIG}" -j10
+run_or_print "${CTEST_CMD}" --test-dir build --build-config "${PRJ1_CONFIG}" -j10
 run_or_print "${CMAKE_CMD}" --install build --config "${PRJ1_CONFIG}"
 
 big_message "prj2 workflow ${PRJ2_WORKFLOW}"
 run_or_print cd "${SOURCE_ROOT_DIR}"/prj2
 run_or_print export CMAKE_BUILD_TYPE="${PRJ2_CONFIG}"
 run_or_print "${CMAKE_CMD}" --preset "${PRJ2_CONFIGURE_PRESET}"
-run_or_print "${CMAKE_CMD}" --build build --config "${PRJ2_CONFIG}"
-run_or_print "${CTEST_CMD}" --test-dir build --build-config "${PRJ2_CONFIG}"
+run_or_print "${CMAKE_CMD}" --build build --config "${PRJ2_CONFIG}" -j10
+run_or_print "${CTEST_CMD}" --test-dir build --build-config "${PRJ2_CONFIG}" -j10
 run_or_print "${CMAKE_CMD}" --install build --config "${PRJ2_CONFIG}"
 
 big_message "Running executable"
