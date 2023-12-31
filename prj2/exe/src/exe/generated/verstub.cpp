5828cdd61bc6113ea919e2ed7ca935535780ea2f
Anthony Dentinger <adentinger.dev@gmail.com>
2023-12-31 00:43:21 -0500

    Delete some files

--- prj2/exe/src/exe/generated/verstub.cpp
+++
@@ -1,8 +0,0 @@
-extern int const sm_version_major = 5;
-extern int const sm_version_minor = 1;
-extern int const sm_version_patch = 0;
-extern char const * const sm_version_git_hash = "49e20cd";
-extern char const * const sm_version_traditional = "5.1.0";
-extern char const * const version_date = "20231228";
-extern char const * const version_time = "04:31:33";
-extern char const * const product_version = "5.1-git-49e20cd";
