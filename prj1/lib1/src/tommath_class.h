2c9635fff492045f2011cbfa67a7afe452788b5b
Anthony Dentinger <adentinger.dev@gmail.com>
2024-01-02 22:20:18 -0500

    Fix linking errors of lib1 globals on Windows

--- prj1/lib1/src/tommath_class.h
+++ prj1/lib1/src/tommath_class.h
@@ -1,3 +1,14 @@
+// https://cmake.org/cmake/help/latest/guide/tutorial/Selecting%20Static%20or%20Shared%20Libraries.html#mathfunctions-mathfunctions-h
+#if defined(_WIN32) && defined(LIB1_SHARED)
+#  if defined(LIB1_EXPORT)
+#    define LTM_SHRSYM __declspec(dllexport)
+#  else
+#    define LTM_SHRSYM __declspec(dllimport)
+#  endif
+#else // non-DLL or static lib
+#  define LTM_SHRSYM
+#endif
+
 #if !(defined(LTM1) && defined(LTM2) && defined(LTM3))
 #if defined(LTM2)
 #define LTM3
