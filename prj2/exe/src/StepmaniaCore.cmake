b0719b31c9c1dc9ea4f11e09f167d282955589a7
Anthony Dentinger <adentinger.dev@gmail.com>
2024-01-09 22:59:30 -0500

    Remove old bzip2 linking code

--- prj2/exe/src/StepmaniaCore.cmake
+++ prj2/exe/src/StepmaniaCore.cmake
@@ -209,11 +209,6 @@ endif()
 find_package(nasm)
 find_package(yasm)
 
-find_package(BZip2)
-if(NOT ${BZIP2_FOUND} AND NOT MSVC)
-  message(FATAL_ERROR "Bzip2 support required.")
-endif()
-
 find_package(Iconv)
 
 find_package(Threads)
